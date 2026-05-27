8bc4a41102790d2670afc5346624bd66e381973e
myusername234457 <afonso@melhoresvendas.com.br>
2026-05-26 23:32:12 -0300

    Add CookieBanner component for cookie consent

---
+++ components/CookieBanner.jsx
@@ -0,0 +1,15 @@
+export default function CookieBanner() {
+  return (
+    <div style={{
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
+      background: "#000",
+      color: "#fff",
+      padding: 10,
+      textAlign: "center"
+    }}>
+      Usamos cookies para melhorar sua experiência
+    </div>
+  );
+}
